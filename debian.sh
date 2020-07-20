fb0c10884b12ae9943f910a83f059fd7f6019e3e
Nigel Agwah <nigel.agwah@gmail.com>
2020-07-20 09:21:53 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/K547NK/debian.git && sh ~/debian/start && sh ~/debian/apps && sh ~/debian/final & rm -rf ~/Scripts
+git clone https://github.com/K547NK/debian.git &&
+cd ~/debian &&
+sh ~/debian/start &&
+sh ~/debian/apps &&
+sh ~/debian/final & rm -rf ~/Scripts
