e7134cd4dcc044c0df1db994c1ec28240bd8e20c
Nigel Agwah <nigel.agwah@gmail.com>
2020-07-26 08:03:13 -0500

    Update low-mem.sh

--- low-mem.sh
+++ low-mem.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 sudo rm -rf /etc/apt/sources.list &&
-sudo touch /etc/apt/sources.list &&
-
-echo "deb http://security.debian.org/debian-security buster/updates main contrib non-free\"deb-src http://security.debian.org/debian-security buster/updates main contrib non-free\"deb http://deb.debian.org/debian/ buster-updates main contrib non-free\"deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free\"deb http://deb.debian.org/debian/ buster main contrib non-free\"deb-src http://deb.debian.org/debian buster main contrib non-free\"deb http://deb.debian.org/debian buster-backports main contrib non-free\"deb-src http://deb.debian.org/debian buster-backports main contrib non-free" >> /etc/apt/sources.list && sudo apt update
+sudo cp sources.list /etc/apt/ &&
 
 # Let's get dependecies for our window manager
 sudo apt update && sudo aptitude install curl firmware-linux-nonfree firmware-linux-free gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev -y &&
@@ -27,10 +25,14 @@ cd .. && rm -rf i3
 sudo aptitude install pulseaudio pavucontrol zathura tty-clock compton-conf xterm htop vim neofetch neomutt isync msmtp pass notmuch mupdf scrot imagemagick polybar font-manager ranger slim compton python3-pip xorg i3lock feh telegram-desktop rofi -y
 sudo apt remove nano vim-tiny -y &&
 
-mkdir -p ~/.config/polybar
-mkdir ~/.config/i3
-mkdir -p ~/.config/rofi/themes
+mkdir -p ~/.config/polybar && mv ~/Scripts/polybar-config ~/.config/polybar/config
+mkdir ~/.config/i3 && mv ~/Scripts/i3-config ~/.config/i3/config
+mkdir -p ~/.config/rofi/themes && mv ~/Scripts/rofi-launcher.rasi ~/.config/rofi/themes/launcher.rasi & mv ~/Scripts/rofi-config ~/.config/rofi/config
+mkdir ~/.config/zathura && mv ~/Scripts/zathurarc ~/.config/zathura/zathurarc
 mkdir ~/Documents &
 mkdir ~/Downloads
 mkdir ~/Music
 mkdir -p ~/Pictures/Screenshots &
+cp ~/Scripts/lock.sh ~/.config/i3/
+cp ~/Scripts/.vimrc ~/
+cp ~/Scripts/.Xresources ~/
