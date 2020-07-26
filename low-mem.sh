7292d14b4c5ac3eb104bd3704ef1d5cd172ff888
Nigel Agwah <nigel.agwah@gmail.com>
2020-07-26 08:28:29 -0500

    Update low-mem.sh

--- low-mem.sh
+++ low-mem.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-sudo rm -rf /etc/apt/sources.list &&
-sudo cp sources.list /etc/apt/ &&
+sudo rm -rf /etc/apt/sources.list 
+sudo cp sources.list /etc/apt/ 
 
 # Let's get dependecies for our window manager
-sudo apt update && sudo aptitude install curl firmware-linux-nonfree firmware-linux-free gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev -y &&
+sudo apt update && sudo aptitude install curl firmware-linux-nonfree firmware-linux-free gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev -y
 
 # Get to cloning and building for the window manager
-git clone https://github.com/resloved/i3.git &&
-cd i3 &&
+git clone https://github.com/resloved/i3.git 
+cd i3 
 
 # compile & install
-autoreconf --force --install &&
+autoreconf --force --install 
 rm -rf build/ &&
-mkdir -p build && cd build/ &&
+mkdir -p build && cd build/ 
 
 # Disabling sanitizers is important for release versions!
 # The prefix and sysconfdir are, obviously, dependent on the distribution.
@@ -34,6 +34,7 @@ mkdir -p ~/Pictures/Screenshots &
 cp ~/Scripts/lock.sh ~/.config/i3/ &
 cp ~/Scripts/.vimrc ~/ &
 cp ~/Scripts/.Xresources ~/ &
+rm -rf ~/Scripts
 
 sudo aptitude install pulseaudio pavucontrol zathura tty-clock compton-conf xterm htop vim neofetch neomutt isync msmtp pass notmuch mupdf scrot imagemagick polybar font-manager ranger slim compton python3-pip xorg i3lock feh telegram-desktop rofi -y
 sudo apt remove nano vim-tiny -y  && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" && sudo reboot
